ca1238c4cfd0113a83a229c66d98b83c3c5836a7
liyananz <517264682@qq.com>
2019-07-12 08:01:10 +0800

    7.12

---
+++ liyn/7.12/Home.vue
@@ -0,0 +1,106 @@
+<<template>
+  <div>
+    <div style="position:fixed;top:0;left:0;background-color:#fff;width:100%;">
+      <nav-bar :data="menu"></nav-bar>
+    </div>
+    <ul class="news-list">
+      <li v-for="(item,index) in newsList" :key="index">
+        <h3>{{item.title}}</h3>
+        <div>
+          <img :src="item.imgs[0]" alt />
+          <img :src="item.imgs[1]" alt />
+          <img :src="item.imgs[2]" alt />
+          <p>
+            <span>{{item.author}}</span>
+            <span>{{item.time}}</span>
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import navBar from "./navBar.vue";
+export default {
+  data() {
+    return {
+      menu: [
+        "推荐",
+        "热门",
+        "科技",
+        "推荐",
+        "热门",
+        "科技",
+        "推荐",
+        "热门",
+        "科技",
+        "娱乐"
+      ],
+      newsList: [
+        {
+          title: "新闻",
+          imgs: ["", "", ""],
+          author: "作者",
+          time: "4天前"
+        },
+        {
+          title: "新闻",
+          imgs: ["", "", ""],
+          author: "作者",
+          time: "4天前"
+        },
+        {
+          title: "新闻",
+          imgs: ["", "", ""],
+          author: "作者",
+          time: "4天前"
+        },
+        {
+          title: "新闻",
+          imgs: ["", "", ""],
+          author: "作者",
+          time: "4天前"
+        },
+        {
+          title: "新闻",
+          imgs: ["", "", ""],
+          author: "作者",
+          time: "4天前"
+        },
+        {
+          title: "新闻",
+          imgs: ["", "", ""],
+          author: "作者",
+          time: "4天前"
+        }
+      ]
+    };
+  },
+  components: {
+    navBar
+  }
+};
+</script>
+<style scoped>
+ul.news-list{
+    padding-top: 50px;
+  }
+ul.news-list li {
+  padding: 10px 20px;
+}
+ul.news-list li h3 {
+  line-height: 1.4em;
+  margin-bottom: 20px;
+}
+ul.news-list li img {
+  width: 30%;
+  height: 100px;
+  border: 1px solid #ccc;
+  margin-right: 3.33%;
+  display: inline-block;
+}
+ul.news-list li p span {
+  margin-right: 10px;
+}
+</style>
