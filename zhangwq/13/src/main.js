e4ddc9203711b0d55e41702c5f9df105e8d239f7
zhangweiqiang123 <2990624558@qq.com>
2019-07-12 08:12:45 +0800

    头条

---
+++ zhangwq/13/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'swiper/dist/css/swiper.min.css'
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
