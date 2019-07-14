c9391ae30c037b07b497f13d4411e6a87ccde934
yuankedi <1596384617@qq.com>
2019-07-14 20:51:47 +0800

    tt

---
+++ yuankd/7.12/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    home
+  }
+}
+</script>
+
+<style>
+#app {
+  width:640px;
+}
+*{
+  margin:0;
+  padding:0;
+}
+body,html,#app{
+  height:100%;
+  overflow:hidden;
+  box-sizing:border-box;
+}
+</style>
