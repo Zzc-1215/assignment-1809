fc144fc129abf65fe0ee417adb2d0e60cc1ddccf
zhengxuemin <2855098938@qq.com>
2019-07-15 23:21:51 +0800

    gx

---
+++ zhengxm/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import home from './components/home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    home
+  },
+}
+</script>
+
+<style>
+*{
+  padding:0;
+  margin:0;
+  box-size:border-box;
+  text-decoration:none;
+}
+body,html,#app{
+  height:100%;
+  overflow:hidden;
+}
+</style>
