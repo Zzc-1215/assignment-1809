8602ceec9fa7a893d5733c1f7e067003b8d808ee
liyananz <517264682@qq.com>
2019-07-15 08:23:21 +0800

    news

---
+++ liyn/今日头条/今日头条/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+   
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import '../icon/iconfont.css'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app{
+  height:100%;
+}
+</style>
