ca8b78b0684b23610749d38dc953206be7b2e4d8
cxywll <1513856399@qq.com>
2019-07-15 08:42:04 +0800

    刷新加载news

---
+++ chenxy/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import home from './components/home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    home
+  }
+}
+</script>
+
+<style>
+*{margin:0;padding:0;list-style:none;}
+body,html,#app {
+  height:100%;
+  overflow: hidden;
+}
+</style>
