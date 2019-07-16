e7ad8df246985ae4166e4e18fbe6224685f59f78
1094141924 <1094141924@qq.com>
2019-07-16 09:23:14 +0800

    7.15

---
+++ caoj/src/components/detail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+       	<router-link :to="{name:'/home'}"><<<<<</router-link>
+		<h1>内容</h1>
+    </div>
+</template>
+<script>
+	
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
