1f65486c9429a58d79674cdc619d552bc41be81d
yangshuo007 <1946085886@qq.com>
2019-07-16 08:03:34 +0800

    7.15

---
+++ yangs/7.15/7.15zuoye/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<script>
+import home from './components/Home.vue'
+export default {
+  name:"app",
+  components:{
+    home
+  }
+}
+</script>
+<style>
+*{margin: 0;
+box-sizing: border-box;
+list-style: none;
+ touch-action: pan-y;
+padding: 0;}
+html,body,#app{
+  overflow:hidden;
+}
+</style>
