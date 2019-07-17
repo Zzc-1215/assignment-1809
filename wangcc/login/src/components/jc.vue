6f6d26eb3f3047432c3de844ad4a5c5305d6a570
wangchengcang <2322305381@qq.com>
2019-07-17 08:24:20 +0800

    login

---
+++ wangcc/login/src/components/jc.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+	  <Vcode  :show="isShow"  @onSuccess="onSuccess"  @onClose="onClose" />
+  </div>
+</template>
+
+<script>
+	import Vcode from "vue-puzzle-vcode";
+  export default {
+    data(){
+      return {
+        isShow: false, // 验证码模态框是否出现
+      }
+    },
+    components:{
+      Vcode
+    },
+    methods:{
+      onSubmit(){
+        this.isShow = true;
+      },
+      // 用户通过了验证
+      onSuccess(msg){
+        this.isShow = false; // 通过验证后，需要手动隐藏模态框
+				console.log(msg)
+      },
+      // 用户点击遮罩层，应该关闭模态框
+      onClose(){
+        this.isShow = false;
+      }
+    }
+  }
+
+</script>
+
+<style>
+</style>
