997eed8d1653d450deff0c943a941e300619cd3f
l1003865808 <1003865808@qq.com>
2019-07-18 08:27:59 +0800

    天气

---
+++ lifx/天气/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div id="nav">
+        <router-view></router-view>
+    </div>
+    
+  </div>
+</template>
+<script>
+import home from './components/home'
+export default {
+  name:'app',
+  components:{
+    home
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box
+}
+#app{
+  width: 100%;
+  height: auto;
+}
+</style>
