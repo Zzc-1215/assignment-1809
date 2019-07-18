65ca99bc213fcf6042770452fa573bc7e5f0c464
Your Name <you@example.com>
2019-07-18 08:38:12 +0800

    moji

---
+++ yangsh/7-16/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import home from './components/HelloWorld.vue'
+export default {
+  name:'app',
+  components:{
+    home
+  }
+}
+</script>
+
+<style>
+*{margin: 0;padding: 0;list-style: none;}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
