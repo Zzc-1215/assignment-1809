287ef8758cc66d70c3dddf9cb00886f137d4f72c
chengtingtinglucky <2426302722@qq.com>
2019-07-18 22:17:24 +0800

    7.18

---
+++ chengtt/vuezuoye/天气预报/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+  
+     <div class="weather-content">
+        <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import '../icon/iconfont.css'
+export default {
+ 
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+a{
+  color: #000;
+}
+body,html{
+  width: 100%;
+  height: 100%;
+}
+#app{
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.weather-content{
+  width: 100%;
+  height: 100%;
+}
+</style>
